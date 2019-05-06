a53288dee4390344f097b094b7088e6a5cf2b605
Alfredo Molina <alfredomolina@Alfredos-MacBook-Pro.local>
2019-05-06 17:02:28 -0400

    my landing so far

---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Landing.css'
+
+
+function Landing() {
+  return (
+    <div>
+    
+<section id="sociallinks">
+<span>
+<img src="" width="" height=""></img>
+<img src="" width="" height=""></img>
+<img src="" width="" height=""></img>
+</span>
+</section>
+    
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default Landing;
